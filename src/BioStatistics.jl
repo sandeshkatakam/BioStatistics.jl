410a0acff232676fe941ceab6b9a140baf17383d
Sandesh Katkaam <sandeshkatakam@gmail.com>
2022-12-25 19:42:28 +0530

    first push

--- src/BioStatistics.jl
+++ src/BioStatistics.jl
@@ -1,5 +1,9 @@
 module BioStatistics
 
 # Write your package code here.
+println("This is the first line of code for this package")
+println("I am writing the first line of code with Gautam Srinivasan (Fkn TAM BRAM) Date: 24/12/2022")
+
+
 
 end
